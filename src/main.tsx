fb8a7be4e3c5972c43bb0334d509b8a4c0c57102
ernikoss <ernikoss93@gmail.com>
2024-05-02 12:42:29 +0200

    Fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const elements = document.getElementsByClassName("threekit-root");
+ReactDOM.createRoot(elements[0]).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
